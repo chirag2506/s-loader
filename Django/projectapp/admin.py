ada84760802269a99d7e47a92ac382ea41355974
jyotikolhe <98251727+jyotikolhe@users.noreply.github.com>
2022-02-10 17:44:42 +0530

    Add files via upload

---
+++ Django/projectapp/admin.py
@@ -0,0 +1,9 @@
+
+
+from import_export.admin import ImportExportModelAdmin
+from django.contrib import admin
+from .models import Person
+
+@admin.register(Person)
+class PersonAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'email', 'location')
