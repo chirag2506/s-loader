ba9d15d81be3e9e074f5b8ba1586a0ce9f9a79ee
shivraj10 <55615387+shivraj10@users.noreply.github.com>
2022-02-15 15:16:36 +0530

    grouping_app

---
+++ grouping/functions/functions.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import csv
+from io import BufferedReader
+def handle_uploaded_file(f):
+    with open('grouping/static/upload'+f.name,'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return destination
+
+
+def get_fields(f):
+   df=pd.read_csv(f.name)
+   #print(list(df.columns))
+   return list(df.columns)
